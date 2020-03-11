464d5e05586c91c5244a94194ffe85d7ea15b50e
humster_spb <you@example.com>
2020-03-11 23:13:49 +0400

    version 11.03 final

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,6 @@
 $(document).ready(function(){
 	
 	$('[name="phone"]').inputmask("+7(999) 999-9999");
-
-	$('.up').click(function (){
-		$('html, body').animate({ scrollTop: 0 }, 1000);
-	});
-	
-	$(window).scroll(function(){
-		if($(this).scrollTop() >= 300){
-			$('.up').css('opacity',0.7);
-		}
-		else {
-			$('.up').css('opacity',0);
-		}
-
-		let scroll = $(this).scrollTop();
-		$('.remont').each(function(){
-			if($(this).offset().top - $(this).outerHeight() <= scroll) {
-				$(this).css('opacity',1);
-			}
-		})
-	});
 	
 	$('.top-slider').slick({
 	  infinite: true,
@@ -65,71 +45,32 @@ $(document).ready(function(){
 	  ] 
 	});
 	
-	$('.bottom-icons').slick({
+	$('.bottom-slider').slick({
 	  infinite: true,
 	  autoplay: true,
-	  autoplaySpeed: 1000,
+	  autoplaySpeed: 1500,
 	  arrows: false,
 	  dots: false,
-	  speed: 1000,
-	  slidesToShow: 8,
-	  slidesToScroll: 3,
+	  speed: 1500,
+	  slidesToShow: 6,
+	  slidesToScroll: 1,
 	  responsive: [
 		{
-		  breakpoint: 1199,
-		  settings: {
-			slidesToShow: 7,
-		  }
-		},
-		{
-		  breakpoint: 991,
+		  breakpoint: 1600,
 		  settings: {
 			slidesToShow: 5,
 		  }
 		},
 		{
-		  breakpoint: 767,
-		  settings: {
-			slidesToShow: 4,
-			slidesToScroll: 2,
-		  }
-		},
-		{
-		  breakpoint: 575,
-		  settings: {
-			slidesToShow: 3,
-			slidesToScroll: 2,
-		  }
-		},
-		{
-		  breakpoint: 480,
-		  settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2,
-		  }
-		}
-	  ] 
-	});
-	
-	$('.part-items').slick({
-	  infinite: true,
-	  autoplay: true,
-	  arrows: true,
-	  dots: false,
-	  speed: 1500,
-	  slidesToShow: 4,
-	  slidesToScroll: 1,
-	  responsive: [
-		{
 		  breakpoint: 1199,
 		  settings: {
-			slidesToShow: 3,
+			slidesToShow: 4,
 		  }
 		},
 		{
 		  breakpoint: 991,
 		  settings: {
-			slidesToShow: 2,
+			slidesToShow: 3,
 		  }
 		},
 		{
@@ -139,7 +80,7 @@ $(document).ready(function(){
 		  }
 		},
 		{
-		  breakpoint: 575,
+		  breakpoint: 630,
 		  settings: {
 			slidesToShow: 1,
 		  }
